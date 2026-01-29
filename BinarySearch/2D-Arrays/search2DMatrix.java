b19b1e87122d0f117d8900d844b1a86303b47b64
Dhruv Sood <dhruvsood1102@gmail.com>
2026-01-29 11:14:31 +0530

    search 2D Matrix- OptimalApproach

--- BinarySearch/2D-Arrays/search2DMatrix.java
+++ BinarySearch/2D-Arrays/search2DMatrix.java
@@ -1,29 +1,54 @@
 public class search2DMatrix {
-    private boolean binarySearch(int[] arr, int x) {
-        int n = arr.length;
-        int low = 0, high = n - 1;
+    public boolean searchMatrix(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0, high = (n * m) - 1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (arr[mid] == x)
+            int row = mid / m;
+            int col = mid % m;
+            if (mat[row][col] == target) {
                 return true;
-            else if (arr[mid] < x) {
+            } else if (mat[row][col] < target) {
                 low = mid + 1;
-            } else {
+            }
+
+            else {
+
                 high = mid - 1;
             }
         }
         return false;
 
     }
-
-    public boolean searchMatrix(int[][] mat, int target) {
-        int n = mat.length;
-        int m = mat[0].length;
-        for (int i = 0; i < n; i++) {
-            if ((mat[i][0] <= target) && (target <= mat[i][m - 1])) {
-                return binarySearch(mat[i], target);
-            }
-        }
-        return false;
-    }
 }
+
+// --------------Better Approach ------------------
+// private boolean binarySearch(int[] arr, int x) {
+// int n = arr.length;
+// int low = 0, high = n - 1;
+// while (low <= high) {
+// int mid = (low + high) / 2;
+// if (arr[mid] == x)
+// return true;
+// else if (arr[mid] < x) {
+// low = mid + 1;
+// } else {
+// high = mid - 1;
+// }
+// }
+// return false;
+
+// }
+
+// public boolean searchMatrix(int[][] mat, int target) {
+// int n = mat.length;
+// int m = mat[0].length;
+// for (int i = 0; i < n; i++) {
+// if ((mat[i][0] <= target) && (target <= mat[i][m - 1])) {
+// return binarySearch(mat[i], target);
+// }
+// }
+// return false;
+// }
