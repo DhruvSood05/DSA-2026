66f857debf525aa88c728c4f71145a79fea5e64d
Dhruv Sood <dhruvsood1102@gmail.com>
2026-02-04 20:35:32 +0530

    Doubly Linked List Array-DoublyLL

---
+++ Linked-List/Fundamentals (Doubly LL)/DlinkedList.java
@@ -0,0 +1,52 @@
+
+class Node {
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data1, Node next1, Node back1) {
+        this.data = data1;
+        this.next = next1;
+        this.back = back1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+        this.back = null;
+    }
+
+}
+
+public class DlinkedList {
+
+    private static void print(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    private static Node arrayToDoublyLinkedList(int[] arr) {
+        // Your code goes here
+        if (arr == null || arr.length == 0)
+            return null;
+        Node head = new Node(arr[0]);
+        Node prev = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
+
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 5, 6, 8 };
+        Node head = arrayToDoublyLinkedList(arr);
+        print(head);
+    }
+}
